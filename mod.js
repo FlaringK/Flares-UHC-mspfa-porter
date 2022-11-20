169d27ee06cffa1187cce3fbcdec7697797c958f
FlaringK <caelans@iinet.net.au>
2022-11-20 21:52:38 +0800

    Updated info

--- mod.js
+++ mod.js
@@ -4,15 +4,18 @@
 // The offical 
 
 // To import your adventure:
-// 1: Use the MSPFA extras script (https://greasyfork.org/en/scripts/396798-mspfa-extras) to download your adventure as a JSON
-// 2: Replace the adventure.json file in this with your json file, and rename it to adventure.json
-// 3: Place all the images or videos used within your adventure in the assets/adventure folder, make sure they are named the same as they are in your adventure, and that they are in the root
-// 4: Edit the mod info in the mod.js folder within the portSettings object
 
-// And that's it!
+// 1. Use the [MSPFA extras script](https://greasyfork.org/en/scripts/396798-mspfa-extras) to download your adventure as a JSON from your adventure info page. You will need to be the editor or owner of the adventure to do this.
+// 2. Replace the adventure.json file in this with your json file, and rename it to adventure.json
+// 3. Place all the images or videos used within your adventure in the assets/adventure folder, make sure they are named the same as they are in your adventure, and that they aren't in any subfolders
+// 4. Edit the mod info in the mod.js folder within the portSettings object
+
+// After that you should have a viewable MSPFA, which can be found at the bottom of the UHC home page.
+
 // You can also add custom CSS in the custom.css file BUT
 // - All adventure CSS should be scoped under .mspfa, I recommend using complied SCSS for this
 // - MSPFA page ranges (i.e. pX-Y classes) will not work
+
 
 // Porter settings
 const portSettings = {
